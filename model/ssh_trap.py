01bb9184f7e0d5f5eac34d37d617555ca5243ebc
YarinGub <yaringub@gmail.com>
2025-08-23 17:19:30 +0300

    ssh trap change

--- model/ssh_trap.py
+++ model/ssh_trap.py
@@ -1,5 +1,5 @@
-
 from model.trap import Trap
+from model.logger import log_event  # ← זה היה חסר
 import time
 
 class SshTrap(Trap):
@@ -9,18 +9,16 @@ class SshTrap(Trap):
     def get_type(self) -> str:
         return "ssh"
 
-    def simulate_interaction(self, input_data: str, ip: str) -> str:
-       log_line = f"[SSH] Interaction from {ip}: {input_data}"
-       print(log_line)
-       return {
-        "trap_type": self.get_type(),
-        "protocol": self.get_protocol(),
-        "ip": ip,
-        "input": input_data,
-        "timestamp": int(time.time()),
-        "data": {
-        "log": log_line
-    }
-}
-
-
+    def simulate_interaction(self, input_data: str, ip: str) -> dict:
+        log_line = f"[SSH] Interaction from {ip}: {input_data}"
+        log_event("honeypot.log", log_line)  # ← הוספנו את זה
+        return {
+            "trap_type": self.get_type(),
+            "protocol": self.get_protocol(),
+            "ip": ip,
+            "input": input_data,
+            "timestamp": int(time.time()),
+            "data": {
+                "log": log_line
+            }
+        }
